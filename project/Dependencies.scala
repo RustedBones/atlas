499364b9af7a8b32e0fd8acee5ae234a2e4fda45
Scala Steward <scala_steward@virtuslab.com>
2024-05-13 18:39:01 +0000

    Update scodec-core to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val FS2      = "3.9.3"
     val MUnitCE3 = "1.0.7"
-    val Scodec   = "2.2.2"
+    val Scodec   = "2.3.0"
   }
 
   val FS2Core    = "co.fs2"     %% "fs2-core"    % Versions.FS2
